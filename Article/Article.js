7cb40aa63f4e598a1c4cb51ad8c7add3dc307f71
cladams <cladams0203@gmail.com>
2019-05-06 17:14:53 -0400

    complete stretch animation on article

--- Article/Article.js
+++ Article/Article.js
@@ -4,10 +4,10 @@ class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
-    // create a reference to the ".expandButton" class. 
+    // create a reference to the ".domElement" class. 
     this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = 'expand';
+    this.expandButton.textContent = 'Click to Expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', () => {
       this.expandArticle();
@@ -17,7 +17,14 @@ class Article {
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle('article-open');
-    this.domElement.classList.toggle('close');
+    if (this.expandButton.textContent == 'Click to Expand') {
+      this.expandButton.textContent = 'Close';
+      this.expandButton.style.color  = 'red';
+    }
+    else if (this.expandButton.textContent == 'Close') {
+      this.expandButton.textContent = 'Click to Expand';
+      this.expandButton.style.color  = 'grey';
+    }
   }
 }
 
